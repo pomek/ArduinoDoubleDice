02538a752fdda46021d11685e082f899125bc0e1
Kamil Piechaczek <pomek@pomek.pl>
2016-09-02 18:51:19 +0200

    Introduced class for management LED connected directly to Arduino.

---
+++ BoardLed.cpp
@@ -0,0 +1,69 @@
+#include <Arduino.h>
+#include "./BoardLed.h"
+
+/**
+ * The default constructor. Allows creating an empty array with
+ * BoardLed objects.
+ * 
+ * @constructor
+ */
+BoardLed::BoardLed() {
+    // This constructor is empty.
+}
+
+/**
+ * @param unsigned short pin Number of pin for controlling the LED.
+ * @constructor
+ */
+BoardLed::BoardLed(const unsigned short pin) {
+    pinMode(pin, OUTPUT);
+
+    this->pin = pin;
+    this->isActive = (HIGH == digitalRead(pin));
+    this->time = millis();
+}
+
+/**
+ * Switch on the LED.
+ * 
+ * @return void
+ */
+void BoardLed::on() {
+    this->isActive = true;
+    digitalWrite(this->pin, HIGH);
+}
+
+/**
+ * Switch off the LED.
+ * 
+ * @return void
+ */
+void BoardLed::off() {
+    this->isActive = false;
+    digitalWrite(this->pin, LOW);
+}
+
+void BoardLed::toggle() {
+    if (this->isActive) {
+        this->off();
+    } else {
+        this->on();
+    }
+}
+
+/**
+ * Allows blinking the LED using a non-blocking interface.
+ * 
+ * @param unsigned int interval The frequency of blinking in mili-seconds.
+ * @return void
+ */
+void BoardLed::blink(const unsigned int interval) {
+    unsigned long currentMillis = millis();
+
+    if (currentMillis - this->time <= interval) {
+        return;
+    }
+
+    this->time = currentMillis;
+
+}
---
+++ BoardLed.h
@@ -0,0 +1,50 @@
+class BoardLed {
+    private:
+        unsigned short pin;
+        unsigned long time;
+        bool isActive;
+
+    public:
+        /**
+         * The default constructor. Allows creating an empty array with
+         * BoardLed objects.
+         * 
+         * @constructor
+         */
+        BoardLed();
+
+        /**
+         * @param unsigned short pin Number of pin for controlling the LED.
+         * @constructor
+         */
+        BoardLed(const unsigned short pin);
+
+        /**
+         * Switch on the LED.
+         * 
+         * @return void
+         */
+        void on();
+
+        /**
+         * Switch off the LED.
+         * 
+         * @return void
+         */
+        void off();
+
+        /**
+         * Switch current status of the LED.
+         *
+         * @return void
+         */
+        void toggle();
+
+        /**
+         * Allows blinking the LED using a non-blocking interface.
+         * 
+         * @param int interval The frequency of blinking in mili-seconds.
+         * @return void
+         */
+        void blink(const unsigned int interval);
+};
